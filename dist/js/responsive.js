0f53edebd5e449258de13bf7a3b0a24abc6b041e
Sergey Khmelevskoy <xs290@me.com>
2018-04-24 05:36:06 +0300

    lk, grades, request, shedule, feedback, ads responsive. autoprefixed main css file

--- dist/js/responsive.js
+++ dist/js/responsive.js
@@ -1489,10 +1489,135 @@ $(document).ready(function(){
     }
   }
 
-
-
-
   initMobileNav();
+
+
+  ////////////
+  // TELEPORT PLUGIN
+  ////////////
+  function initTeleport(){
+    $('.js-teleport').each(function (i, val) {
+      var self = $(val)
+      var objHtml = $(val).html();
+      var target = $('[data-teleport-target=' + $(val).data('teleport-to') + ']');
+      var conditionMedia = $(val).data('teleport-condition').substring(1);
+      var conditionPosition = $(val).data('teleport-condition').substring(0, 1);
+
+      if (target && objHtml && conditionPosition) {
+
+        function teleport() {
+          var condition;
+
+          if (conditionPosition === "<") {
+            condition = _window.width() < conditionMedia;
+          } else if (conditionPosition === ">") {
+            condition = _window.width() > conditionMedia;
+          }
+
+          if (condition) {
+            console.log(target)
+            target.html(objHtml)
+            self.html('')
+          } else {
+            self.html(objHtml)
+            target.html("")
+          }
+        }
+
+        teleport();
+        _window.on('resize', debounce(teleport, 200));
+
+
+      }
+    })
+  }
+
+  initTeleport();
+
+
+  /// GRADES TABLE
+  if (  $('.es-progress__tab-body-item').length > 0 ){
+    $('.es-progress__tab-body-item').each(function(i, table){
+      var table = $(table);
+      var head = table.find('.es-progress__line:first-child .es-progress__line-item');
+      var headNames = [];
+      var rows = table.find('.es-progress__line:not(:first-child)');
+      head.each(function(i, tr){
+        headNames.push( $(tr).html() )
+      })
+      rows.each(function(i, tr){
+        var td = $(tr).find('.es-progress__line-item')
+
+        td.each(function(i, td){
+          // ignore first (tr header)
+          if ( i == 0 ){
+            return
+          }
+          $(td).prepend('<span class="for-mobile">'+ headNames[i] +'</span>')
+        })
+      })
+    });
+  }
+
+  /// REQUEST TABLE
+  if (  $('.request__list').length > 0 ){
+    $('.request__list').each(function(i, table){
+      var table = $(table);
+      var head = table.find('.request__list-header div');
+      var headNames = [];
+      var rows = table.find('.request__list-line');
+      head.each(function(i, tr){
+        headNames.push( $(tr).html() )
+      })
+      console.log(headNames)
+      rows.each(function(i, tr){
+        var td = $(tr).find('div')
+        td.each(function(i, td){
+          // ignore first (tr header)
+          if ( i == 0 ){
+            return
+          }
+          $(td).prepend('<span class="for-mobile">'+ headNames[i] +'</span>')
+        })
+      })
+    });
+  }
+
+  // SHEDULE TABLE COLORS
+  if ( $('.table_lessons').length > 0 ) {
+    function setTableColors(){
+      $('.table_lessons td').each(function(i, td){
+        var bg = $(td).css('background-color');
+        var prevSibl = $(td).prev()
+        console.log(bg)
+        if (  bg != "rgb(255, 255, 255)" && bg != "rgb(2, 24, 43)" ){
+          // copy bg color
+          if ( _window.width() < 767 ){
+            prevSibl.css({
+              'background-color': bg
+            })
+          } else {
+            prevSibl.css({
+              'background-color': "rgb(255, 255, 255)"
+            })
+          }
+        }
+      })
+    }
+
+    setTableColors();
+    _window.on('resize', debounce(setTableColors, 200));
+
+
+  }
+
+
+
+
+  // ADS REMOVE NSBP
+  if ( $('.appeal__content__head_contact').length > 0 ){
+    // $('.appeal__content__head_contact').html().replace(/\u00A0/g, '')
+  }
 
 
 });
